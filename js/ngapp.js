b533b23c6dccbf399fead21b76605816dd4f274f
Blake Feggans <bfeggans@gmail.com>
2014-11-17 00:57:43 -0500

    Cleaned up file structure and added functionality for searching by sport button

--- js/ngapp.js
+++ js/ngapp.js
@@ -1,1 +1,24 @@
-var app = angular.module('App', [])
+angular.module('App', [])
+	.controller('TileController', function ($scope, $http, $filter){
+
+	$http.get('output.json').
+  	success(function(data, status) {
+	    $scope.scores = data;
+		  console.log(status);
+	});
+
+	$scope.buttons = [{sport: 'NFL'},{sport: 'NCAA FB'},{sport: 'NBA'},{sport: 'NCAAB'},{sport: 'MLB'}, {sport: 'NHL'}];
+
+	$scope.sportClick = function(button) {
+    $scope.selected = button;
+  }
+
+  $scope.isSelected = function(button) {
+    return $scope.selected === button;
+  }
+
+	$scope.something = function(text) {
+		console.log(text.substr(4,2));
+		return text.substr(4,2);
+	}
+})
--- js/ngcontroller.js
+++
@@ -1,8 +0,0 @@
-app.controller('TileController', function ($scope, $http){
-
-	$http.get('output.json').
-  		success(function(data, status) {
-		    $scope.scores = data;
-		    console.log(status);
-		})
-})
