fee70825cc58d61030186f491be32b7a13c5c49d
Blake Feggans <bfeggans@gmail.com>
2014-11-17 21:10:19 -0500

    Changes include basic animations, better button functionality, and in progress toggling.

--- js/ngapp.js
+++ js/ngapp.js
@@ -1,24 +1,52 @@
-angular.module('App', [])
-	.controller('TileController', function ($scope, $http, $filter){
+angular.module('App', ['ngAnimate'])
+	.controller('TileController', function ($scope, $http, $filter, $timeout){
 
-	$http.get('output.json').
-  	success(function(data, status) {
-	    $scope.scores = data;
-		  console.log(status);
-	});
+	$scope.getScores = function () {
+		$http.get('output.json').
+				success(function(data, status) {
+		    	return $scope.scores = data;
+			  	console.log(status);
+				});
+			}
+
+		$scope.getScores();
 
 	$scope.buttons = [{sport: 'NFL'},{sport: 'NCAA FB'},{sport: 'NBA'},{sport: 'NCAAB'},{sport: 'MLB'}, {sport: 'NHL'}];
 
 	$scope.sportClick = function(button) {
     $scope.selected = button;
+		$scope.getScores();
   }
 
   $scope.isSelected = function(button) {
     return $scope.selected === button;
   }
 
-	$scope.something = function(text) {
-		console.log(text.substr(4,2));
-		return text.substr(4,2);
+	$scope.inProgressGames = function(scores) {
+		var inProgress = [];
+		for (var i = 0; i < scores.length; i++) {
+			if (scores[i].timeRem.length === 6) {
+				if (!(scores[i].timeRem.substr(4,2) == 'PM') && !(scores[i].timeRem == 'Final')){
+					inProgress.push(scores[i]);
+				}
+			} else
+				if (!(scores[i].timeRem.substr(5,2) == 'PM') && !(scores[i].timeRem == 'Final')){
+					inProgress.push(scores[i]);
+				}
+		}
+		$scope.scores = inProgress;
+		return $scope.scores;
 	}
+
+	var called = false;
+
+	$scope.toggle = function (scores) {
+		if (called) { called = false; return $scope.getScores(); }
+		$scope.inProgressGames(scores);
+		called = true;
+	}
+
+
+	//$scope.inProgressGames();
+
 })
